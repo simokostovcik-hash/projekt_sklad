21de2c96c65d277d398a189f82d38ed73f77cd37
simokostovcik-hash <simo.kostovcik@gmail.com>
2026-02-24 09:10:50 +0100

    commit4

--- src/main/java/cz/projekt_sklad/App.java
+++ src/main/java/cz/projekt_sklad/App.java
@@ -1,13 +1,9 @@
 package cz.projekt_sklad;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
     }
 }
